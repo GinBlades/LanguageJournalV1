9057c510413c11aee021423b96330e48061d4529
Shaun Martin <martins@surgery.wisc.edu>
2016-11-18 14:59:25 -0600

    Angular 2! j/k

--- src/LanguageJournal/gulpfile.js
+++ src/LanguageJournal/gulpfile.js
@@ -1,15 +1,32 @@
 ﻿var gulp = require("gulp"),
-    concat = require("gulp-concat"),
-    sourcemaps = require("gulp-sourcemaps");
+    sourcemaps = require("gulp-sourcemaps"),
+    ts = require("gulp-typescript");
 
-gulp.task("copyNpm", () => {
-    gulp.src(["node_modules/@angular/**/*"])
-        .pipe(gulp.dest("wwwroot/@angular"))
-
-    return gulp.src(["node_modules/rxjs/**/*"])
-        .pipe(gulp.dest("wwwroot/rxjs"));
+// This doesn't finish properly when the task claims to be finished.
+// There is probably a way I need to modify this so that it works properly with streams.
+gulp.task("copyNpm", ["systemSetup"], () => {
+    ["@angular", "rxjs", "core-js", "zone.js", "reflect-metadata", "systemjs"].forEach((folder) => {
+        gulp.src([`node_modules/${folder}/**/*`])
+            .pipe(gulp.dest(`wwwroot/${folder}`));
+    });
 });
 
-gulp.task('default', function () {
+gulp.task("systemSetup", () => {
+    return gulp.src(["Client/systemjs.config.js"])
+        .pipe(gulp.dest("wwwroot"));
+})
+
+gulp.task("tsc", () => {
+    let tsProject = ts.createProject("tsconfig.json");
+
+    let tsResult = gulp.src("Client/**/*.ts")
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(gulp.dest("wwwroot"));
+});
+
+gulp.task("build", ["copyNpm", "tsc"]);
+
+gulp.task('default', ["build"], () => {
     // place code for your default task here
 });
